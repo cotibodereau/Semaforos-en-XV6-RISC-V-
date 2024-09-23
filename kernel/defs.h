d5de2c7ec0c397e0b3314603d1a270b320ff8182
nic <nicolle.ferreyra53@gmail.com>
2024-09-23 11:07:35 -0300

    agrego def de sem_open

--- kernel/defs.h
+++ kernel/defs.h
@@ -185,5 +185,9 @@ void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
 void            virtio_disk_intr(void);
 
+//semaforos.c
+int             sem_open(int, int);
+
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
